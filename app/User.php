f24ce1ad5ccc3deee361654214a5568161be6f56
HAMMAD ALLAUDDIN <hammadallauddin@gmail.com>
2019-07-21 01:36:20 +0500

    all work

---
+++ app/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+         'email',
+         'password',
+         'role_id',
+         'is_active',
+         'photo_id',
+         'password_confirmation'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    public function role(){
+      return $this->belongsTo('App\Role');
+    }
+
+    public function photo(){
+      return $this->belongsTo('App\Photo');
+    }
+
+    public function isAdmin(){
+      if($this->role->name == 'Administrator' && $this->is_active){
+        return true;
+      }
+      return false;
+    }
+
+    public function posts(){
+      return $this->hasMany('App\Post');
+    }
+}
